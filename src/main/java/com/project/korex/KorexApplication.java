9c51b59ea2c6be71d15c5ba8d90b16def4805974
Patrick <jjgus29@naver.com>
2025-07-18 11:25:39 +0900

    feat: init main

---
+++ src/main/java/com/project/korex/KorexApplication.java
@@ -0,0 +1,13 @@
+package com.project.korex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/korex/KorexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.korex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
